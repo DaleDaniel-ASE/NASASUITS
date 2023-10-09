311256b6db2491c28d81d43288c718a4a1feaf6b
Jordan Thomas Decena Acampado <143011848+j1acampado@users.noreply.github.com>
2023-10-09 10:55:03 -0500

    Changes

--- Python_Thomas/Thomas_jyShell.py
+++ Python_Thomas/Thomas_jyShell.py
@@ -1,45 +1,51 @@
-# -*- coding: utf-8 -*-
+## Jordan Thomas Acampado
+## ASE 374K 
 
 import pygame as pg
+clock = pg.time.Clock()
 
 pg.init()
 
-#SCENE 1 ______________________________________________________________________________
-screen = pg.display.set_mode((700, 467))
-clock = pg.time.Clock()
+#Define Shell for Pygame Engine (Replace with Quest AR Engine)
+screen = pg.display.set_mode((1500, 600)) 
+
 running = True
 dt = 0 
 
 
-        
-def redrawGameWindow1():
- 
-count = 0
-
-while running
-    redrawGameWindow1()
-    
-
-#Initiailize Classes and Images
+#Initiailize Classes and Images 
 pg.display.set_caption('image')
-    #Background
-battle =  pg.image.load("/Users/dindoacampado/Downloads/bg_bdaman2.png").convert()
-battle = pg.transform.scale(battle,(700,467))
 
+class map(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
+          
+    def draw(self,screen):
+       surface = pg.display.set_mode((self.x,self.y))
+       color = (255,0,0)
+       pg.draw.rect(surface, color, pg.Rect(30, 30, 60, 60))
 
+    #Background 
+background =  pg.image.load("Python_Thomas/Mars_back.webp").convert()
+#background = pg.transform.scale(background,(700,467))
         
 
 def redrawGameWindow():
+    mapb.draw(screen)
     pg.display.update()
 
-while running
-    
-#EVENTS__________________________________________________________________
+
+mapb = map(0, 0) 
+## Main Function
+while running:   
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False 
         
+## Display Mars Background (Replace with AR Field)
     screen.fill("black")
+    screen.blit(background,(0,0)) 
 
     dt = clock.tick(30)/1000
     redrawGameWindow()
