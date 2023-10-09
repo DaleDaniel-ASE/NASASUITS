4e69d81bd9354474c67257ba9e877576ba40d046
Jordan Thomas Decena Acampado <143011848+j1acampado@users.noreply.github.com>
2023-10-09 14:18:18 -0500

    Developing

--- Python_Thomas/Thomas_jyShell.py
+++ Python_Thomas/Thomas_jyShell.py
@@ -7,36 +7,74 @@ clock = pg.time.Clock()
 pg.init()
 
 #Define Shell for Pygame Engine (Replace with Quest AR Engine)
-screen = pg.display.set_mode((1500, 600)) 
-
+screen = pg.display.set_mode((1200, 600)) 
 running = True
 dt = 0 
 
 
-#Initiailize Classes and Images 
+
+#Image Defitions
+
+background =  pg.image.load("Python_Thomas/Mars_back.webp").convert()
+#background = pg.transform.scale(background,(700,467))
+window = pg.image.load("Python_Thomas/Rect.png").convert_alpha()
+slider = pg.image.load("Python_Thomas/slider.png").convert_alpha()
+
+#Initiailize Classes
 pg.display.set_caption('image')
 
 class map(object):
     def __init__(self,x,y):
         self.x = x
-        self.y = y
-          
+        self.y = y 
+      
     def draw(self,screen):
-       surface = pg.display.set_mode((self.x,self.y))
-       color = (255,0,0)
-       pg.draw.rect(surface, color, pg.Rect(30, 30, 60, 60))
+       mapa = pg.transform.scale(window,(200,200))
+       mapa.set_alpha(128)
+       screen.blit(mapa, (self.x, self.y))
 
-    #Background 
-background =  pg.image.load("Python_Thomas/Mars_back.webp").convert()
-#background = pg.transform.scale(background,(700,467))
+class vitals(object):
+    def __init__(self,x,y, oxygen, power):
+        self.x = x
+        self.y = y 
+        self.oxygen = oxygen
+        self.power = power 
+    def draw(self,screen):
+       oxy = pg.transform.scale(window,(20,400))
+       oxy.set_alpha(100)
+       oxymeter = pg.transform.scale(slider,(20, (self.oxygen/100)*400))
+       oxymeter.set_alpha(100)
+       screen.blit(oxy, (self.x, self.y))
+       screen.blit(oxymeter, (self.x, self.y))
+
+       volt = pg.transform.scale(window,(20,400))
+       volt.set_alpha(100)
+       voltmeter = pg.transform.scale(slider,(20, (self.power/100)*400))
+       voltmeter.set_alpha(100)
+       screen.blit(volt, (self.x + 30, self.y))
+       screen.blit(voltmeter, (self.x + 30, self.y))
+
+class ui(object):
+    def __init__(self,x,y):
+        self.x = x
+        self.y = y
         
-
+    def draw(self,screen):
+        color = (255,0,0)
+        lg1 = pg.draw.rect(screen, color, pg.Rect(600, 600, 60, 60))
+        
+        
+#Function Defintiions
 def redrawGameWindow():
     mapb.draw(screen)
+    vital.draw(screen)
     pg.display.update()
 
+#Define UI Elements
 
-mapb = map(0, 0) 
+mapb = map(30, 30)
+vital = vitals(30,200, 30, 50)
+
 ## Main Function
 while running:   
     for event in pg.event.get():
